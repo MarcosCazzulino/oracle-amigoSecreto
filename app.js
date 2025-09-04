96e0cb10d9c327ff786f36b7056bd8e04b1aa200
Marcos Cazzulino <marcoscazzulino@gmail.com>
2025-09-04 02:29:46 -0300

    funcionalidad sortear amigo

--- app.js
+++ app.js
@@ -3,28 +3,48 @@ let amigos = [];
 
 const ingresoNombres = document.querySelector('#amigo');
 const listaAmigos = document.querySelector('#listaAmigos');
+const amigoSorteado = document.getElementById('resultado');
 
 function validacion(){
-    if (ingresoNombres.value.trim() === ''){
-        alert('Debes ingresar un nombre.');
-        ingresoNombres.value = '';
-    }
+    alert('Debes ingresar un nombre.');
+    ingresoNombres.value = '';
 }
 
 function actualizarLista(){
+    amigoSorteado.innerHTML = '';
+
     amigos.push(ingresoNombres.value);
-    listaAmigos.innerHTML = ''
+    listaAmigos.innerHTML = '';
     for (let amigo in amigos){
         const liAmigo = document.createElement('li');
-        
+
         liAmigo.innerHTML = amigos[amigo];
         listaAmigos.appendChild(liAmigo);
     }
 }
 
 function agregarAmigo(){
-    validacion();
-    actualizarLista()
-    console.log(amigos)
+    if (ingresoNombres.value.trim() === ''){
+        validacion();
+    } else{
+        actualizarLista();
+    }
+    console.log(amigos);
     ingresoNombres.value = '';
+}
+
+function sortearAmigo(){
+    if (amigos.length > 0){
+        const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        console.log(indiceAleatorio)
+        const amigoSecreto = amigos[indiceAleatorio];
+
+        const liSorteado = document.createElement('li');
+        liSorteado.innerHTML = `El amigo secreto es ${amigoSecreto}`;
+
+        amigoSorteado.appendChild(liSorteado);
+
+        listaAmigos.innerHTML = '';
+        amigos = [];
+    }
 }

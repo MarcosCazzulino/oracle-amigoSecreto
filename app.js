445162457f5b1cf1c693da5099c08c2bd74f96dc
Marcos Cazzulino <marcoscazzulino@gmail.com>
2025-09-04 02:34:29 -0300

    documentación de código

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
 let amigos = [];
 
 const ingresoNombres = document.querySelector('#amigo');
 const listaAmigos = document.querySelector('#listaAmigos');
 const amigoSorteado = document.getElementById('resultado');
 
+// Función que se aplica en caso de que el input esté vacío
 function validacion(){
     alert('Debes ingresar un nombre.');
     ingresoNombres.value = '';
 }
 
 function actualizarLista(){
+    // Acá limpio la ul donde se muestra el amigo sorteado, para poder mostrar una nueva lista
     amigoSorteado.innerHTML = '';
 
     amigos.push(ingresoNombres.value);
@@ -24,19 +26,18 @@ function actualizarLista(){
 }
 
 function agregarAmigo(){
+    // Acá agrego la función trim() para que los nombres que sólo son espacios vacíos, tampoco sean válidos
     if (ingresoNombres.value.trim() === ''){
         validacion();
     } else{
         actualizarLista();
     }
-    console.log(amigos);
     ingresoNombres.value = '';
 }
 
 function sortearAmigo(){
     if (amigos.length > 0){
         const indiceAleatorio = Math.floor(Math.random() * amigos.length);
-        console.log(indiceAleatorio)
         const amigoSecreto = amigos[indiceAleatorio];
 
         const liSorteado = document.createElement('li');
@@ -44,7 +45,10 @@ function sortearAmigo(){
 
         amigoSorteado.appendChild(liSorteado);
 
+        // Acá limpio la lista de amigos mostrada en pantalla, una vez que se sortea el amigo secreto
         listaAmigos.innerHTML = '';
+
+        // Finalmente, vacío el array para poder generar una nueva lista
         amigos = [];
     }
 }

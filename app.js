1f55c865d4daf0a35f0a3797a5356311e137bd93
Marcos Cazzulino <marcoscazzulino@gmail.com>
2025-09-04 01:45:21 -0300

    Validación entrada de nombres

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+const ingresoNombres = document.querySelector('#amigo');
+
+if (ingresoNombres.value.trim() === ''){
+    alert('Debes ingresar un nombre.');
+    ingresoNombres.value = '';
+}
+

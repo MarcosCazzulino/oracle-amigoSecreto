31e426460d3435f13d1a9ca2730e206f24c8b05d
Marcos Cazzulino <marcoscazzulino@gmail.com>
2025-09-04 01:47:29 -0300

    Función agregar nombres y mostrar en pantalla

--- app.js
+++ app.js
@@ -2,9 +2,21 @@
 let amigos = [];
 
 const ingresoNombres = document.querySelector('#amigo');
+const listaAmigos = document.querySelector('#listaAmigos');
 
-if (ingresoNombres.value.trim() === ''){
-    alert('Debes ingresar un nombre.');
+function agregarAmigo(){
+    if (ingresoNombres.value.trim() === ''){
+        alert('Debes ingresar un nombre.');
+        ingresoNombres.value = '';
+    } else{
+        amigos.push(ingresoNombres.value);
+        const liAmigo = document.createElement('li');
+        const nombreAmigo = document.createElement('p');
+
+        nombreAmigo.innerHTML = ingresoNombres.value;
+        liAmigo.appendChild(nombreAmigo);
+        listaAmigos.appendChild(liAmigo);
+    }
+    console.log(amigos)
     ingresoNombres.value = '';
 }
-
